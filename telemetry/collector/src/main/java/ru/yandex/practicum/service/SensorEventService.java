1301735158a69f5a580c97adc3522da68bfaf33b
BelovEvgenii <Zhekab777@yandex.ru>
2026-06-25 15:51:24 +0300

    feat: Создать мапперы для REST DTO (HubEventDtoMapper, SensorEventDtoMapper) Реализовать отправку REST событий в Kafka (логирование) Добавить обработку ошибок при отправке в Kafka

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/SensorEventService.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/SensorEventService.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.config.kafka.KafkaEventProducer;
 import ru.yandex.practicum.dto.sensor.SensorEventDto;
-import ru.yandex.practicum.mapper.SensorEventMapper;
+import ru.yandex.practicum.mapper.SensorEventDtoMapper;
 import telemetry.service.collector.SensorEventProto;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import ru.yandex.practicum.mapper.SensorEventMapper;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +20,8 @@ import telemetry.service.collector.SensorEventProto;
 public class SensorEventService {
 
     final KafkaEventProducer kafkaProducer;
-    final SensorEventMapper mapper;
+    final SensorEventMapper protoMapper;
+    final SensorEventDtoMapper dtoMapper;
 
     @Value("${kafka.topics.sensor-events:telemetry.sensors.v1}")
     String sensorEventsTopic;
@@ -28,7 +31,18 @@ public class SensorEventService {
                 event.getId(), event.getHubId(), event.getType());
 
         try {
-            log.info("Событие датчика получено: {}", event);
+            SensorEventAvro avroEvent = dtoMapper.toAvro(event);
+
+            if (avroEvent == null) {
+                log.error("Ошибка конвертации события датчика в Avro: id={}", event.getId());
+                throw new RuntimeException("Ошибка конвертации события датчика");
+            }
+
+            String key = avroEvent.getHubId();
+            kafkaProducer.send(sensorEventsTopic, key, avroEvent);
+
+            log.info("Событие датчика успешно отправлено в Kafka: id={}, hubId={}",
+                    event.getId(), event.getHubId());
         } catch (Exception e) {
             log.error("Ошибка при обработке события датчика: id={}", event.getId(), e);
             throw new RuntimeException("Не удалось обработать событие датчика", e);
@@ -40,7 +54,7 @@ public class SensorEventService {
                 event.getId(), event.getHubId(), event.getPayloadCase());
 
         try {
-            var avroEvent = mapper.toAvro(event);
+            SensorEventAvro avroEvent = protoMapper.toAvro(event);
 
             if (avroEvent == null) {
                 log.error("Ошибка конвертации события датчика в Avro: id={}", event.getId());
@@ -50,8 +64,8 @@ public class SensorEventService {
             String key = avroEvent.getHubId();
             kafkaProducer.send(sensorEventsTopic, key, avroEvent);
 
-            log.info("Событие датчика успешно отправлено в Kafka: id={}, hubId={}, ключ={}",
-                    event.getId(), event.getHubId(), key);
+            log.info("Событие датчика успешно отправлено в Kafka: id={}, hubId={}",
+                    event.getId(), event.getHubId());
         } catch (Exception e) {
             log.error("Ошибка при обработке события датчика: id={}", event.getId(), e);
             throw new RuntimeException(String.format("Не удалось обработать событие датчика: %s", event.getId()), e);
